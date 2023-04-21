d75e693c2e6367202350e2f50750a097a91349c5
JezvCP <jezv@yandex.ru>
2023-04-21 17:48:42 +0800

    fix settings.json

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 
-from dotenv import load_dotenv
 from naff import Intents
 from naff.ext.debug_extension import DebugExtension
 
@@ -11,24 +10,15 @@ from core.base import CustomClient
 from core.extensions_loader import load_extensions
 import openai
 
-with open('keys.json') as keys_file:
-    keys = json.load(keys_file)
-    config = {
-        'bot_name': 'Бот-обормот',
-        'admin_role': 'Тимлид',
-        'bot_image_url': 'https://media.discordapp.net/attachments/1046772205876424794/1092428709300011039/CineOP_dumpling_simplified_logo_simple_forms_a7fd2df5-94e4-4538-b193-e55fa249702a.png?width=676&height=676',
-        'server_meet_url': 'https://discord.gg/2HK5XBfMkn',
-        'openai-key': keys['openai-key'],
-        'bot-token': keys['bot-token'],
-        'debug-mode': keys['debug-mode']
-    }
 
 if __name__ == "__main__":
-    # load the environmental vars from the .env file
-    load_dotenv()
+    # get keys and tokens for a bot
+    with open('keys.json') as keys_file:
+        settings = json.load(keys_file)
+    config = settings['config']
 
     #Init ChatGPT key
-    openai.api_key = config['openai-key']
+    openai.api_key = settings['openai-key']
     # initialise logging
     init_logging()
 
@@ -40,11 +30,11 @@ if __name__ == "__main__":
     )
 
     # load the debug extension if that is wanted
-    if config['debug-mode'] == "true":
+    if settings['debug-mode'] == "true":
         DebugExtension(bot=bot)
 
     # load all extensions in the ./extensions folder
     load_extensions(bot=bot, config=config)
 
     # start the bot
-    bot.start(config['bot-token'])
+    bot.start(settings['bot-token'])
