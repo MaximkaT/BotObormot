037a8424d900ba796c68837b4ecd70700177105e
JezvCP <jezv@yandex.ru>
2023-04-21 22:43:41 +0800

    fix path

--- utils/BingAI.py
+++ utils/BingAI.py
@@ -1,9 +1,14 @@
 from EdgeGPT import Chatbot, ConversationStyle
+import pathlib as pl
+import json
 
 async def bing_chat(prompt="", style=ConversationStyle.balanced):
     # Функция получения ответа от BingAI с использованием cookies.
     if prompt == "":
         return "Пустой запрос, повторите"
-    gbot = Chatbot(cookie_path=r'C:\Projects\BotObormot\utils\cook.json')
+    path = pl.Path(pl.Path.cwd(), 'utils', 'cook.json')
+    with open(path) as file:
+        data = json.load(file)
+    gbot = Chatbot(cookies=data)
     response_dict = await gbot.ask(prompt=prompt, conversation_style=style)
     return response_dict['item']['messages'][1]['text'].replace("[^\\d^]", "")
