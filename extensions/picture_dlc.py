ca55c2b1a414f40f3b1def8825033909bfe720ea
CineOP <57627686+CineOP@users.noreply.github.com>
2023-05-04 06:24:29 +0800

    Added /imagine Kandinsky and /imagine stableDiff

--- extensions/picture_dlc.py
+++ extensions/picture_dlc.py
@@ -8,14 +8,15 @@ from naff import (
     SlashCommandChoice, OptionTypes, Context,
     AutoArchiveDuration, Message,
     ThreadChannel,
-    User, EmbedField
+    User, EmbedField, EmbedAttachment
 )
 class Midjourney(Extension):
     
     def __init__(self, bot, config):
         self.bot = bot
         self.CONFIG = config
-        self.replica = Replica()
+        self.kand = Replica(url='kandinsky-2')
+        self.diff = Replica(url='stable-diffusion')
 
 
     @slash_command(
@@ -30,7 +31,35 @@ class Midjourney(Extension):
         opt_type=OptionTypes.STRING,
         required=True
     )
-    async def kandinskiy(self):
+    async def kandinskiy(self, ctx: InteractionContext, *, prompt: str):
+        await ctx.defer()
+        urle = self.kand.imagine(prompt)[0]
+        imag_emb = EmbedAttachment(urle)
+        q_embed_author = EmbedAuthor(name=str(ctx.author), icon_url=ctx.author.avatar.as_url(size=128))
+        emb = Embed(title=prompt, image=imag_emb, color=(255, 255, 255), author=q_embed_author)
+        await ctx.send(f'{ctx.author.mention}', embed=emb)
+    
+
+
+    @slash_command(
+            name="imagine",
+            description="Создание изображения по текстовому запросу.",
+            sub_cmd_name="diffusion",
+            sub_cmd_description="Рисование изображения по запросу с помощью нейросети Stable Diffusion",
+    )
+    @slash_option(
+        name="prompt",
+        description="Опишите изображение",
+        opt_type=OptionTypes.STRING,
+        required=True
+    )
+    async def kandinskiy(self, ctx: InteractionContext, *, prompt: str):
+        await ctx.defer()
+        urle = self.diff.imagine(prompt)[0]
+        imag_emb = EmbedAttachment(urle)
+        q_embed_author = EmbedAuthor(name=str(ctx.author), icon_url=ctx.author.avatar.as_url(size=128))
+        emb = Embed(title=prompt, image=imag_emb, color=(255, 255, 255), author=q_embed_author)
+        await ctx.send(f'{ctx.author.mention}', embed=emb)
         
 
 
--- utils/Kandinsky.py
+++ utils/Kandinsky.py
@@ -3,13 +3,15 @@ import pathlib as pl
 import json
 
 class Replica:
-    def __init__(self, url='ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f'):
+    def __init__(self, url='kandinsky-2'):
+        urls = {'kandinsky-2': 'ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f',
+                'stable-diffusion': 'stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf'}
         path = pl.Path(pl.Path.cwd(),'keys.json')
         with open(path) as keys_file:
             data = json.load(keys_file)
             
         self.client = replicate.Client(api_token=data['replica-token'])
-        self.url = url
+        self.url = urls.get(url)
     
     def imagine(self, prompt):
         output = self.client.run(
